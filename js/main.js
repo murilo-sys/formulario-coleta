642959127a601cd37f1a795944054630502e7ac6
Murilo <murilod_santiago@hotmail.com>
2026-05-25 16:25:32 -0300

    feat: adicionado novas funcionalidades

--- js/main.js
+++ js/main.js
@@ -133,8 +133,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const formulario = document.querySelector('.formularioColeta')
 
   if (formulario) {
+
+    //Clique do botão submit
     formulario.addEventListener('submit', (evento) => {
       evento.preventDefault();
+
+      console.log("botão clicado")
+
+      //Limpar campos erro-input
+      const todosOsCampos = document.querySelectorAll('input, select');
+      todosOsCampos.forEach(campo => {
+        campo.addEventListener('input', () => {
+          campo.classList.remove('erro-input');
+        });
+      });
+
 
       //Puxando os valores sem a mascara
       const cnpjSolicitante = maskSolicitante ? maskSolicitante.unmaskedValue : '';
@@ -142,6 +155,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const documentoRemetente = maskRemetente ? maskRemetente.unmaskedValue : '';
       const cepLimpo = maskCep ? maskCep.unmaskedValue : '';
       const pesoLimpo = maskPeso ? maskPeso.unmaskedValue : '';
+      const valorNfLimpo = maskValor ? maskValor.unmaskedValue : '';
 
       //Função verificar se documento (CNPJ/CPF) está valido
       function DocValido(doc) {
@@ -153,24 +167,109 @@ document.addEventListener('DOMContentLoaded', () => {
 
       //verifica solicitante
       if (!DocValido(cnpjSolicitante)) {
-        alert("Cnpj do solicitante invalido")
-        return
+        void solicitanteDoc.offsetWidth
+        solicitanteDoc.classList.add('erro-input')
       }
 
       //verifica remetente
       if (!DocValido(documentoRemetente)) {
-        alert("Cnpj do Remetente invalido")
-        return
+        void docRemetente.offsetWidth
+        docRemetente.classList.add('erro-input')
       }
 
       //verifica destinatario
       if (!DocValido(documentoDestinatario)) {
-        alert("Cnpj do Destinatario invalido")
-        return
+        void docDestinatario.offsetWidth
+        docDestinatario.classList.add('erro-input')
       }
 
-      
+      //verifica natureza da mercadoria
+      const naturezaMercadoria = document.getElementById('naturezaMercadoria')
+      if (naturezaMercadoria.value == "") {
+        void naturezaMercadoria.offsetWidth
+        naturezaMercadoria.classList.add('erro-input')
+      }
 
+      //verifica se valornf é menor ou igual que zero || ou nulo
+      if (valorNfLimpo <= 0 || isNaN(valorNfLimpo) || valorNfLimpo == "") {
+        void valorNf.offsetWidth
+        valorNf.classList.add('erro-input')
+      }
+
+      //verifica se quantidade de volumes é valido
+      const qtdVolumes = document.getElementById('qtdVolumes')
+      if (qtdVolumes.value <= 0 || isNaN(qtdVolumes.value) || qtdVolumes.value == "") {
+        void qtdVolumes.offsetWidth
+        qtdVolumes.classList.add('erro-input')
+      }
+
+      //verifica se peso real é valido
+      if (pesoLimpo <= 0 || isNaN(pesoLimpo) || pesoLimpo == "") {
+        void pesoReal.offsetWidth
+        pesoReal.classList.add('erro-input')
+      }
+
+      //Se caso tiver erro, ir para ele e clicar no campo
+      const primeiroErro = document.querySelector('.erro-input')
+      if (primeiroErro) {
+        primeiroErro.scrollIntoView({ behavior: 'smooth', block: 'center' })
+        primeiroErro.focus();
+        return
+      }
     })
+
+
+    //Adiciona nova dimensão 
+    const btnAdicionar = document.getElementById('botaoAdicionar');
+    const containerCubagem = document.getElementById('containerCubagem');
+
+    if (btnAdicionar && containerCubagem) {
+      btnAdicionar.addEventListener('click', () => {
+
+        const qtdColunaCubagem = document.querySelectorAll('.coluna-cubagem')
+        const qtdVolumes = document.getElementById('qtdVolumes')
+
+        if ((qtdColunaCubagem.length) >= qtdVolumes.value) {
+          void qtdVolumes.offsetWidth
+          qtdVolumes.classList.add('erro-input')
+          return
+        }
+
+
+        // O bloco de HTML para duplicar
+        const novaLinhaHTML = `
+        <div class="coluna-cubagem">
+          <div class="coluna-flex-1">
+            <p class="pergunta-cubagem">COMPRIMENTO</p>
+            <div class="input-grupo-sufixo">
+              <input class="input-comprimento" placeholder="0">
+              <span class="sufixo">| cm</span>
+            </div>
+          </div>
+          <div class="coluna-flex-1">
+            <p class="pergunta-cubagem">LARGURA</p>
+            <div class="input-grupo-sufixo">
+              <input class="input-largura" placeholder="0">
+              <span class="sufixo">| cm</span>
+            </div>
+          </div>
+          <div class="coluna-flex-1">
+            <p class="pergunta-cubagem">ALTURA</p>
+            <div class="input-grupo-sufixo">
+              <input class="input-altura" placeholder="0">
+              <span class="sufixo">| cm</span>
+            </div>
+          </div>
+          <div class="coluna-lixo">
+            <button class="botao-lixo" type="button">🗑️</button>
+          </div>
+        </div>
+        `;
+
+        // Injeta o HTML novo no final da lista dentro do container
+        containerCubagem.insertAdjacentHTML('beforeend', novaLinhaHTML);
+
+      });
+    }
   }
 });
